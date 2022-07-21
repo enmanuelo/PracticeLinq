d74d758b4e204e59cfeec655ed28871afbbb7696
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-21 14:38:05 -0400

    rewrote code into one concise line on line 6

--- PracticeLinq/Program.cs
+++ PracticeLinq/Program.cs
@@ -1,22 +1,14 @@
 ﻿using System;
 using System.Linq;
 
-/* DONE - Create a new console app and name it PracticeLinq.
-   DONE - Create a list of video game names...
-   DONE - Order the list of games by length of the game name.
-   DONE - Use the lambda expression in this exercise as well.
-
-        use Method Syntax for this exercise
-*/
-
-
 List<string> videoGames = new List<string>() {"Madden", "NBA 2K 2022", "GTA", "Mario"};
 
-IEnumerable<string> orderGames =
-    videoGames.OrderBy(x => x.Length);
+videoGames.OrderBy(x => x).ToList().ForEach(x => Console.WriteLine(x));
 
-foreach (var item in orderGames)
-{
-    Console.WriteLine(item);
-}
+//IEnumerable<string> orderGames =
+//    videoGames.OrderBy(x => x.Length);
 
+//foreach (var item in orderGames)
+//{
+//    Console.WriteLine(item);
+//}
